523541fbd6d246b0ef4b3870b12ae42a1ba80c67
MMKD <ms.mdn.das@gmail.com>
2025-09-07 13:35:41 +0400

    Add project files.

---
+++ BookSrore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookSrore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
