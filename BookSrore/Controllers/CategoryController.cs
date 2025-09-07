71ccaecd8deb5b9e08298a9cb57edc3c10c135d3
MMKD <ms.mdn.das@gmail.com>
2025-09-07 18:18:02 +0400

    AddThemeAndCreateCategory

---
+++ BookSrore/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using BookSrore.Data;
+using BookSrore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookSrore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var categoryList = _context.Categories.ToList();
+            return View(categoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category model)
+        {
+            if (!ModelState.IsValid)
+            {
+
+            }
+
+            _context.Categories.Add(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
